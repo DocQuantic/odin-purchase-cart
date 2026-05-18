f435aeea620518c76074b9b3362ed468d1484e0f
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2026-05-18 20:58:06 +0200

    Add Total field in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -13,43 +13,64 @@ const Cart = () => {
         setCart(cart.filter(item => item.id != productId))        
     }
 
-    function increaseValue(){
-        
+    function increaseValue(e){
+        const itemId = e.target.parentNode.parentNode.className.split(' ')[1].split('-')[1]
+        const newCart = cart.map(cartItem => cartItem.id == itemId ? {id: itemId, quantity: (parseInt(cartItem.quantity) + 1).toString()} : cartItem)
+        setCart(newCart)
     }
 
-    function decreaseValue(){
-        
+    function decreaseValue(e){
+        const itemId = e.target.parentNode.parentNode.className.split(' ')[1].split('-')[1]
+        if(cart.filter(cartItem => cartItem.id == itemId)[0].quantity > 1){
+            const newCart = cart.map(cartItem => cartItem.id == itemId ? {id: itemId, quantity: (parseInt(cartItem.quantity) - 1).toString()} : cartItem)
+            setCart(newCart)
+        }
+    }
+
+    if(cart.length != 0){
+        return (
+            <>
+                <h1>Cart</h1>
+                <div className={styles.cart}>
+                    {cart.map(cartItem => {
+                        const product = products.filter(product => product.id == cartItem.id)[0]
+                        return (
+                            <div key={cartItem.id} className={styles.cartItem + ' id-' + cartItem.id}>
+                                <h2>{product.title}</h2>
+                                <div className={styles.quantity}>
+                                    <button onClick={decreaseValue}>-</button>
+                                    <input type="number" value={cartItem.quantity} readOnly/>
+                                    <button onClick={increaseValue}>+</button>
+                                </div>
+                                <p>{product.price} $</p>
+                                <button onClick={removeCartItem}>Remove from cart</button>
+                            </div>
+                        )
+                    })}
+                    <div className={styles.total}>
+                        <p>
+                            Total : {
+                                cart.reduce((totalPrice, cartItem) => {
+                                    const itemPrice = products.filter(product => product.id == cartItem.id)[0].price
+                                    return totalPrice + itemPrice * cartItem.quantity
+                                }, 0)
+                            } $
+                        </p>
+                    </div>
+                </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h1>Cart</h1>
+                <div className={styles.cart}>
+                    <p>Your cart is empty</p>
+                </div>
+            </>
+        )
     }
     
-    return (
-        <>
-            <h1>Cart</h1>
-            <div className={styles.cart}>
-                {cart.map(cartItem => {
-                    console.log(cartItem)
-                    const product = products.filter(product => product.id == cartItem.id)[0]
-                    console.log(product)
-                    return (
-                        <div key={cartItem.id} className={styles.cartItem}>
-                            <h2>{product.title}</h2>
-                            <div className={styles.quantity}>
-                                <button onClick={decreaseValue}>-</button>
-                                <input type="number" value={cartItem.quantity} readOnly/>
-                                <button onClick={increaseValue}>+</button>
-                            </div>
-                            <p>{product.price} $</p>
-                            <button onClick={removeCartItem}>Remove from cart</button>
-                        </div>
-                    )
-                })}
-                <div className={styles.total}>
-                    <p>
-                        Total : $
-                    </p>
-                </div>
-            </div>
-        </>
-    )
 }
 
 export default Cart
